cf5a01a784410ad881dabb0ca33aac1c64e7b0d4
Kellojo <kellojo1@gmail.com>
2018-02-02 11:59:02 +0100

    Added Word Count (php side)

--- php/loadTable.php
+++ php/loadTable.php
@@ -7,6 +7,7 @@
 
 	/* Get all tables for a given user */
 	$rows = array();
+	$count = 0;
 
 	$stmt = $connection->prepare('SELECT word, description, source, words.wID FROM words JOIN sources on words.sID = sources.sID JOIN tables on words.tID = tables.tID WHERE tables.tID = ? && tables.oID = ?');
 	$stmt->bind_param('ss', $_POST["tID"], $_SESSION["id"]);
@@ -15,6 +16,16 @@
 	while($row = mysqli_fetch_assoc($result)) {
 		array_push ($rows, $row);
 	};
+
+	$stmt = $connection->prepare('SELECT count(*) FROM words JOIN sources on words.sID = sources.sID JOIN tables on words.tID = tables.tID WHERE tables.tID = ? && tables.oID = ?');
+	$stmt->bind_param('ss', $_POST["tID"], $_SESSION["id"]);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	while($row = mysqli_fetch_assoc($result)) {
+		$count = $row;
+	};
+
+
 	$connection->close();
 	echo json_encode($rows);
 	die();
