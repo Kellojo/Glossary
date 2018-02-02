56f743190c01a99dc8cf4550d27210ffa2825c7e
Kellojo <kellojo1@gmail.com>
2018-02-02 12:05:27 +0100

    Update loadTable.php

--- php/loadTable.php
+++ php/loadTable.php
@@ -7,7 +7,6 @@
 
 	/* Get all tables for a given user */
 	$rows = array();
-	$count = 0;
 
 	$stmt = $connection->prepare('SELECT word, description, source, words.wID FROM words JOIN sources on words.sID = sources.sID JOIN tables on words.tID = tables.tID WHERE tables.tID = ? && tables.oID = ?');
 	$stmt->bind_param('ss', $_POST["tID"], $_SESSION["id"]);
@@ -16,15 +15,6 @@
 	while($row = mysqli_fetch_assoc($result)) {
 		array_push ($rows, $row);
 	};
-
-	$stmt = $connection->prepare('SELECT count(*) FROM words JOIN sources on words.sID = sources.sID JOIN tables on words.tID = tables.tID WHERE tables.tID = ? && tables.oID = ?');
-	$stmt->bind_param('ss', $_POST["tID"], $_SESSION["id"]);
-	$stmt->execute();
-	$result = $stmt->get_result();
-	while($row = mysqli_fetch_assoc($result)) {
-		$count = $row;
-	};
-
 
 	$connection->close();
 	echo json_encode($rows);
