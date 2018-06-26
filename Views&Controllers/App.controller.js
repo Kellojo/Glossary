dfeb50ce0ef5868a2f5a3d916172242130fc1866
Kellojo <kellojo1@gmail.com>
2018-06-26 23:27:40 +0200

    upload

---
+++ Views&Controllers/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast",
+   "sap/m/IconTabBar",
+   "sap/m/IconTabFilter",
+   "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, IconTabBar, IconTabFilter, JSONModel) {
+   "use strict";
+
+
+   return Controller.extend("Glossary.App", {
+
+   });
+});
